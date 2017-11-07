a1e7944b44cb6560b773b2496f4994293bba1f8b
Ryan J. Hodge <ryanjhodge@users.noreply.github.com>
2017-11-07 13:21:15 -0700

    Increase popup maxHeight

--- src/util/util.js
+++ src/util/util.js
@@ -166,7 +166,7 @@ module.exports = {
     return map.getContainer().clientWidth - 120;
   },
   _getAvailableVerticalSpace: function (map) {
-    return map.getContainer().clientHeight - 352;
+    return map.getContainer().clientHeight - 300;
   },
   _lazyLoader: require('./lazyloader.js'),
   _parseLocalUrl: function (url) {
